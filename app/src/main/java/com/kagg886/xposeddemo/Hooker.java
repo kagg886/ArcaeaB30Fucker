1d6403f1cb26db5caf2a6729f76d979eed941c3f
kagg886 <iveour@163.com>
2022-11-13 20:14:52 +0800

    XposedInit

---
+++ app/src/main/java/com/kagg886/xposeddemo/Hooker.java
@@ -0,0 +1,11 @@
+package com.kagg886.xposeddemo;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class Hooker implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        
+    }
+}
